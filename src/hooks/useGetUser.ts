939963ee498bf197d5da6f1ec0a56945c5f603a1
amos-mustfin <k.amos@mufin.co.kr>
2024-08-06 14:05:47 +0300

    Fixed Next.js server issues

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -2,8 +2,10 @@
 
 import { UserInterface } from "@/api/hooks";
 
-export const useGetUser = (): UserInterface => {
-    const user = typeof window !== 'undefined' && localStorage.getItem('user');
-    
-    return user ? JSON.parse(user) : {};
-};
+export const useGetUser = (): UserInterface | {} => {
+    if (typeof window !== 'undefined') {
+        const user = localStorage.getItem('user');
+        return user ? JSON.parse(user) : {};
+    }
+    return {}; 
+}
